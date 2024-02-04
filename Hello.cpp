1b34c3cc315da96bcb2a7fa5e984f8f9da3e7845
Isaac <158229593+BigFrito@users.noreply.github.com>
2024-02-03 17:24:35 -0800

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,1 +1,14 @@
+# Auto detect text files and perform LF normalization
+* text=auto
+/***
+ * {Lab 1}
+ ***/
 
+ #include <iostream>
+ #include <cstdlib>
+ using namespace std;
+
+ int main(){
+    cout << "Hello World!...Hello World again!\n" >>
+ }
+ 
